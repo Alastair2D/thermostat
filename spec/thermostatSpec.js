d0cb5d553ad1cfc2d75c5c6e2cb20937b8ea7312
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-06 14:21:42 +0100

    passing test for decrease

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -2,19 +2,30 @@ describe('Thermostat', function () {
   var t1;
 
   beforeEach(function () {
-    t1 = new Thermostat();
-  });
+    t1 = new Thermostat()
+  })
 
   describe('#new', function() {
     it('defaults temperature at 20', function() {
-      expect(t1.temperature).toEqual(20);
-    });
-  });
+      expect(t1.temperature).toEqual(20)
+    })
+  })
 
   describe('#up', function(){
     it('increases temperature by 1', function(){
-      t1.up();
-      expect(t1.temperature).toEqual(21);
+      t1.up()
+
+      expect(t1.temperature).toEqual(21)
     })
   })
+
+  describe('#down', function() {
+    it('decreases temperature by 1', function() {
+      t1.down()
+
+      expect(t1.temperature).toEqual(19)
+    })
+  })
+
 })
+
