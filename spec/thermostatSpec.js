c5d9d1ee209a0b21b50efd782208461f4ecd913c
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-06 14:15:55 +0100

    add up test

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,29 +1,20 @@
-describe("Thermostat", function() {
+describe('Thermostat', function () {
   var t1;
 
-beforeEach(function() {
-  t1 = new Thermostat();
+  beforeEach(function () {
+    t1 = new Thermostat();
+  });
+
+  describe('#new', function() {
+    it('defaults temperature at 20', function() {
+      expect(t1.temperature).toEqual(20);
+    });
+  });
+
+  describe('#up', function(){
+    it('increases temperature by 1', function(){
+      t1.up();
+      expect(t1.temperature).toEqual(21);
+    })
+  })
 })
-
-describe('#new', function() {
-  it('default starts at 20 degrees'), function() {
-    expect(t1.temperature).toEqual(20)  // Need red braces afer temperature?
-  }
-})
-
-// describe('#up', function() {
-//   it('increases the temperature by 1'), function() {
-//     expect(t1.up()).toEqual(21)
-//   }
-// })
-
-//   describe('#down', function () {
-//     it('decreases the temperature by 1'), function () {
-//       expect(t1.down()).toEqual(19)
-//     }
-//   })
-
-
-
-
-});
