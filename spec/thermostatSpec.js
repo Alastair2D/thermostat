29f6be93958dd365fdcbae70c64d33ebea09a7de
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-06 14:57:58 +0100

    add powerSave

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -5,27 +5,34 @@ describe('Thermostat', function () {
     t1 = new Thermostat()
   })
 
-  describe('#new', function() {
-    it('defaults temperature at 20', function() {
+  describe('#new', function () {
+    it('defaults temperature at 20', function () {
       expect(t1.temperature).toEqual(20)
+    })
+    it('defaults with powerSave as on/true', function() {
+      expect(t1.powerSave).toEqual(true)
     })
   })
 
-  describe('#up', function(){
-    it('increases temperature by 1', function(){
+  describe('#up', function () {
+    it('increases temperature by 1', function () {
       t1.up()
 
       expect(t1.temperature).toEqual(21)
     })
   })
 
-  describe('#down', function() {
-    it('decreases temperature by 1', function() {
+  describe('#down', function () {
+    it('decreases temperature by 1', function () {
       t1.down()
 
       expect(t1.temperature).toEqual(19)
     })
+    it("doesn't decrease temperature below MINIMUM_TEMPERATURE", function () {
+      t1.temperature = 10;
+      t1.down
+      expect(t1.temperature).toEqual(10) // expect(function () { t1.down() }).toThrow("can't");
+    });
   })
 
 })
-
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,14 +1,21 @@
 function Thermostat() {
-  this.temperature = 20
+  this.DEFAULT_TEMPERATURE = 20
+  this.MINIMUM_TEMPERATURE = 10
+  this.temperature = this.DEFAULT_TEMPERATURE
+  this.powerSave = true 
 }
 
-Thermostat.prototype.up = function() {
-  this.temperature ++                             //+=1
+Thermostat.prototype.up = function () {
+  this.temperature++                             //+=1
 }
 
-Thermostat.prototype.down = function() {
-  this.temperature --
+Thermostat.prototype.down = function () {
+  if (this.temperature > this.MINIMUM_TEMPERATURE) {
+    this.temperature--
+  } else {
+    return
+  }
+
+  // (this.temperature > this.MINIMUM_TEMPERATURE ? this.temperature -- : throw "can't")
 }
-
-
 
