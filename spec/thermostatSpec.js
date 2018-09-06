6afa0fc2cec8c7cc00f46a4280410dc9bf13b6f5
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-06 14:01:23 +0100

    tidy

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,23 +7,21 @@ beforeEach(function() {
 
 describe('#new', function() {
   it('default starts at 20 degrees'), function() {
-    expect(t1.temperature()).toEqual(20)  // Need red braces afer temperature?
+    expect(t1.temperature).toEqual(20)  // Need red braces afer temperature?
   }
 })
 
-// describe('MINIMUM')
+// describe('#up', function() {
+//   it('increases the temperature by 1'), function() {
+//     expect(t1.up()).toEqual(21)
+//   }
+// })
 
-describe('#up', function() {
-  it('increases the temperature by 1'), function() {
-    expect(t1.up()).toEqual(21)
-  }
-})
-
-  describe('#down', function () {
-    it('decreases the temperature by 1'), function () {
-      expect(t1.down()).toEqual(19)
-    }
-  })
+//   describe('#down', function () {
+//     it('decreases the temperature by 1'), function () {
+//       expect(t1.down()).toEqual(19)
+//     }
+//   })
 
 
 
