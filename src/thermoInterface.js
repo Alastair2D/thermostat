70ed431857d5602bd57929d8acc447613228c0f1
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-07 10:00:08 +0100

    refactor to showTemp

--- src/thermoInterface.js
+++ src/thermoInterface.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
   thermostat = new Thermostat();
-  $('#temperature').text(thermostat.temperature)
+  showTemperature()
 })
 
 // $(document).ready(function () {
 //   $('#temperature').text('hello jquery world');
 // })
 
+function showTemperature() {
+  $('#temperature').text(thermostat.temperature)
+}
+
+
 $('#temp_up').click(function() {
   thermostat.up()
-  $('#temperature').text(thermostat.temperature)
+  showTemperature()
 })
 
 
