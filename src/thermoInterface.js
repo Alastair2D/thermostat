ed1c503db514f2edfe82cdcbc4baf260ccd82172
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-07 12:30:47 +0100

    api working

--- src/thermoInterface.js
+++ src/thermoInterface.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+})
+  $('#current-city').change(function () {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function (data) {
+      $('#current-temperature').text(data.main.temp)
+    })
+  })
   thermostat = new Thermostat();
   showTemperature()
 })
+
 
 function showTemperature() {
   $('#temperature').text(thermostat.temperature)
